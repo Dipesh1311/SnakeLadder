1f35de5a331e2f399453bf1de3ad864b7eb174f9
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-07-29 21:03:33 +0530

    Added implemented program of UC2 for Rolling Dice.

--- src/com/bridgelabz/snakeladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeAndLadder.java
@@ -1,8 +1,14 @@
 package com.bridgelabz.snakeladder;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int player1 = 0;
+        int Roll_Die;
         System.out.println("Player1 at " +player1+ " position");
+        Random a = new Random();
+        Roll_Die = a.nextInt(6) + 1;
+        System.out.println(Roll_Die);
     }
 }
