44cb9f655937de66341c596263e890e2ecf06eda
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-07-29 20:58:02 +0530

    Added implemented program of UC1 for start of snake and ladder game.

---
+++ src/com/bridgelabz/snakeladder/SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.snakeladder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int player1 = 0;
+        System.out.println("Player1 at " +player1+ " position");
+    }
+}
