fa501e4600c273a133016d1a739c62bc42d563c6
Dipesh1311 <dipesh.rathod794@gmail.com>
2022-07-29 21:20:55 +0530

    Added implemented program of UC3 to get the position of player1.

--- src/com/bridgelabz/snakeladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeladder/SnakeAndLadder.java
@@ -5,10 +5,42 @@ import java.util.Random;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int player1 = 0;
-        int Roll_Die;
+        int Roll_Dice;
+        int check;
+        final int No_Play = 0;
+        final int Ladder = 1;
+        final int Snake = 2;
+        int position = 0;
+
         System.out.println("Player1 at " +player1+ " position");
         Random a = new Random();
-        Roll_Die = a.nextInt(6) + 1;
-        System.out.println(Roll_Die);
+        Roll_Dice = a.nextInt(6) + 1;
+        System.out.println(Roll_Dice);
+        check = a.nextInt(3);
+        System.out.println(check);
+
+        switch (check) {
+
+            case   No_Play:
+                System.out.println("NOPLAY");
+                System.out.println("Position of player : " + position);
+                System.out.println("----------------------");
+                break;
+            case Ladder:
+                System.out.println("############## Climb up the ladder ############");
+                position += Roll_Dice;
+                System.out.println("Position of the  Player = " + position);
+                System.out.println("----------------------");
+                break;
+            case Snake:
+                System.out.println("~~~~~~~~~~~~~~~~ Swallowed by snake ~~~~~~~~~~~~");
+                position = position + Roll_Dice;
+                System.out.println("Position of the  Player = " + position);
+                System.out.println("----------------------");
+                break;
+
+            default:
+                break;
+        }
     }
 }
